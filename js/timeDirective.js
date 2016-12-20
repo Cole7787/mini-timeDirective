179c825605be9f09f01600d758c9d67c878b4e04
Cole Allan <cacoleallan@gmail.com>
2016-12-20 11:54:38 -0700

    Updated date filter on output

--- js/timeDirective.js
+++ js/timeDirective.js
@@ -2,7 +2,7 @@ angular.module('timeApp')
 .directive('showTime', function(){
   return {
     restrict: 'E',
-    template: '<div> The current time is {{time}} </div>',
+    template: '<div> The current time is {{time | date: "short"}} </div>',
     link: function(scope, element, attrs){
       var currentTime = new Date();
       scope.time = currentTime.toString();
